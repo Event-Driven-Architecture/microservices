160da50e54a56ff6521e01d132efee98f35c6109
Elimane Fofana <elimane.fofana@supinfo.com>
2022-11-21 05:29:27 +0100

    all

---
+++ kafka-to-elastic-service/src/test/java/com/microservices/demo/kafkatoelasticservice/KafkaToElasticServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.demo.kafkatoelasticservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaToElasticServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
